e19fbdc3d635f61afee873a5efee4994e5f055d3
ShijunW <shijun.wang6@gmail.com>
2018-09-04 17:07:04 -0700

    day6

---
+++ src/getters.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from collections import Counter
+
+def get_business(user_id):
+    '''
+    return business reviewed by a given user_id
+    INPUT: string
+    OUTPUT: pandas DataFrame
+    '''
+    # get all restaurants reviewed by the user_id
+    row_list = []
+    for id in review['business_id']:
+        business_cursor = db.business.find({'business_id': id})
+        row_list.append(list(business_cursor))
+    # convert to pandas DataFrame
+    business = pd.DataFrame()
+    for i in range(len(row_list)):
+    business = pd.concat([business, pd.DataFrame(row_list[i])], ignore_index=True)
+    return business
+
+def get_category_words(user_id):
+    '''
+    return category words for a given user_id
+    INPUT: string
+    OUTPUT: dict
+    '''
+    business = get_business(user_id)
+    cat_words = []
+    for i in range(len(business)):
+        for word in business.categories[i].split(', '):
+            cat_words.append(word)
+
+    words_count = Counter(cat_words)
+    word_dict = dict(words_count.most_common())
+    return word_dict
+
+
--- src/mongo.py
+++ src/mongo.py
@@ -1,3 +1,27 @@
-import sys, urllib2, json, pymongo
+import sys, json, pymongo
 from pymongo import MongoClient
 
+
+def read_json(file_name):
+    '''
+    Read the json file into a list
+    INPUT: string
+    OUTPUT: list
+    '''
+    with open(file_name, 'r') as json_file:
+        list_json = json_file.readlines()
+    return list_json
+
+def json_to_mongo(file_name, db_name, collection_name):
+    '''
+    Read json file and store to mongodb
+    INPUT: string, string, string
+    OUTPUT: None
+    '''
+    list_json = read_json(file_name)
+    client = MongoClient()
+    db = client[db_name]
+    collection = db[collection_name]
+    for json_item in list_json:
+        record = json.loads(json_item)
+        collection.insert_one(record)
